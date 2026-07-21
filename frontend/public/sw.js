1c0cf382d80ab7d1663371e4f7074065feb58b08
xprss <vincenzo.sagristano@yahoo.com>
2026-07-21 17:27:58 +0000

    fix: serve PWA manifest as json

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "huff-shell-v1";
-const APP_SHELL = ["/", "/manifest.webmanifest", "/icons/huff-icon.svg"];
+const CACHE_NAME = "huff-shell-v2";
+const APP_SHELL = ["/", "/manifest.json", "/icons/huff-icon.svg"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
